0f06dbaa32299f82e727a5f04370d4ef264f24f4
Abhishek Jindal <abhishekjindal.jindal74@gmail.com>
2022-07-04 06:17:37 +0000

    Day 14: data sources: get vpc with tags

--- day_14/subnet.tf
+++ day_14/subnet.tf
@@ -1,18 +1,15 @@
-variable "vpc_id" {
-    type = string
-    description = "The id of the VPC you want to refer in the data block."
-    # default = "vpc-0711c86d"
-}
-
 data "aws_vpc" "vpc" {
-  id = var.vpc_id
+    filter {
+        name = "tag:Environment"
+        values = ["prod"]
+    }
 }
 
 data "aws_subnets" "subnets" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.vpc.id]
-  }
+    filter {
+        name   = "vpc-id"
+        values = [data.aws_vpc.vpc.id]
+    }
 }
 
 output "subnet-ids" {
